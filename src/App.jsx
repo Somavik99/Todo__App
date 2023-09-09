8e373b950e39976163f626d83b58eadd54d340f2
Avik Roy <somavik99@gmail.com>
2023-09-09 12:42:23 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import CounterApp from "./Components/CounterApp"
+import "./App.css";
 
 import TodoApp from "./Components/CartApp/TodoApp";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl text-center font-bold underline">Todo App</h1>
       <div>
         <TodoApp />
       </div>
--- src/Components/CartApp/TodoApp.jsx
+++ src/Components/CartApp/TodoApp.jsx
@@ -1,9 +1,34 @@
-
+import Light from "../../images/bg-desktop-light.jpg";
+// import {IoIosRadioButtonOff} from "react-icons/io"
 
 const TodoApp = () => {
   return (
-    <div>CartApp</div>
-  )
-}
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="relative">
+          <div>
+            <img src={Light} alt="Light" className="w-[100%]" />
+          </div>
+          <div className="absolute top-[5%] left-0 right-0 bottom-0 flex justify-center items-center align-middle flex-col">
+            <h1 className="mr-[12%] absolute top-12 right-auto   text-[2.5em] text-white mb-8">
+              T O D O
+            </h1>
+            <div>
+              {/* <IoIosRadioButtonOff className="absolute m-[14px]"/> */}
+              <input
+                type="text"
+                placeholder="⚪  Create new todo"
+                className="border-[3px]  ml-28  outline-none rounded-[10px] h-8 p-[1.2em] w-[26vw]"
+              />
+            </div>
+            <div>
 
-export default TodoApp
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default TodoApp;
